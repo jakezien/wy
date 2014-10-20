793d6c88d9ab1c061e60154277c4ce827c2e50fc
Jake Zien <jake@floored.com>
2014-10-19 23:44:24 -0400

    Something

--- _assets/js/views/Home.js
+++ _assets/js/views/Home.js
@@ -36,10 +36,44 @@ define([
       var createTopTL = function(i, el) {
         var tl = new TimelineLite({paused: true});
         tl.to(this.$el.find('.bg video'), 5, {opacity:0});
+        tl.to(this.$el.find('#intro .text'), 5, {color:'#F22E60'}, '-=5');
+        tl.to(this.$el.find('#intro .keyline'), 5, {backgroundColor:'#F22E60'}, '-=5');
+        // tl.set(this.$el.find('.bg video'), {display: 'block'});
+        tl.call(function(){
+          this.$el.find('.bg video')[0].play();
+        }.bind(this));
+        // tl.set(this.$el.find('.bg video'), {display: 'none'});
+        tl.call(function(){
+          this.$el.find('.bg video')[0].pause();
+        }.bind(this));
+        
+
         return tl;
       }.bind(this);
 
+      var createQerosTL = function(i, el) {
+        var tl = new TimelineLite({paused: true});
+        tl.to(this.$el.find('#home-qeros .caption'), 5, {transform:'translateY(40vh)'}, Sine.easeInOut);
+        return tl;
+      }.bind(this);
+
+      var createProjectsTL = function(i, el) {
+        var tl = new TimelineLite({paused: true});
+        tl.to(this.$el.find('#home-projects .caption'), 5, {transform:'translateY(40vh)'}, Sine.easeInOut);
+        return tl;
+      }.bind(this);
+
+      var createSchoolsTL = function(i, el) {
+        var tl = new TimelineLite({paused: true});
+        tl.to(this.$el.find('#home-schools .caption'), 5, {transform:'translateY(40vh)'}, Sine.easeInOut);
+        return tl;
+      }.bind(this);
+
+
       this.timelines['top'] = createTopTL();
+      this.timelines['home-qeros'] = createQerosTL();
+      this.timelines['home-projects'] = createProjectsTL();
+      this.timelines['home-schools'] = createSchoolsTL();
     },
 
     render: function(currentScrollY) {
