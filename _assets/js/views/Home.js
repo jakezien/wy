aea42997c55a6ef08196628623949f9a4697b6df
Jake Zien <jake@floored.com>
2014-10-25 19:09:56 -0400

    More home page work.

--- _assets/js/views/Home.js
+++ _assets/js/views/Home.js
@@ -53,27 +53,47 @@ define([
 
       var createQerosTL = function(i, el) {
         var tl = new TimelineLite({paused: true});
-        tl.to(this.$el.find('#home-qeros .caption'), 5, {transform:'translateY(40vh)'}, Sine.easeInOut);
+        tl.to(this.$el.find('#home-qeros .caption'), 5, {transform:'translateY(50vh)'}, Sine.easeOut);
         return tl;
       }.bind(this);
 
       var createProjectsTL = function(i, el) {
         var tl = new TimelineLite({paused: true});
-        tl.to(this.$el.find('#home-projects .caption'), 5, {transform:'translateY(40vh)'}, Sine.easeInOut);
+        tl.to(this.$el.find('#home-projects .caption'), 5, {transform:'translateY(50vh)'}, Sine.easeOut);
         return tl;
       }.bind(this);
 
       var createSchoolsTL = function(i, el) {
         var tl = new TimelineLite({paused: true});
-        tl.to(this.$el.find('#home-schools .caption'), 5, {transform:'translateY(40vh)'}, Sine.easeInOut);
+        tl.to(this.$el.find('#home-schools .caption'), 5, {transform:'translateY(50vh)'}, Sine.easeOut);
         return tl;
       }.bind(this);
 
+      var createAboutTL = function(i, el) {
+        var tl = new TimelineLite({paused: true});
+        tl.to(this.$el.find('#home-about .caption'), 5, {transform:'translateY(50vh)'}, Sine.easeOut);
+        return tl;
+      }.bind(this);
+
+      var createShopTL = function(i, el) {
+        var tl = new TimelineLite({paused: true});
+        tl.to(this.$el.find('#home-shop .caption'), 5, {transform:'translateY(50vh)'}, Sine.easeOut);
+        return tl;
+      }.bind(this);
+
+      var createDonateTL = function(i, el) {
+        var tl = new TimelineLite({paused: true});
+        tl.to(this.$el.find('#home-donate .caption'), 5, {transform:'translateY(50vh)'}, Sine.easeOut);
+        return tl;
+      }.bind(this);
 
       this.timelines['top'] = createTopTL();
       this.timelines['home-qeros'] = createQerosTL();
       this.timelines['home-projects'] = createProjectsTL();
       this.timelines['home-schools'] = createSchoolsTL();
+      this.timelines['home-about'] = createAboutTL();
+      this.timelines['home-shop'] = createShopTL();
+      this.timelines['home-donate'] = createDonateTL();
     },
 
     render: function(currentScrollY) {
