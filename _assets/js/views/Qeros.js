40a1d89b55a9ee0a93c0bcae2dd90918b757423a
Jake Zien <jake@floored.com>
2014-11-29 13:49:32 -0500

    Video edits; more Qeros tweaks; start of mailchimp.

--- _assets/js/views/Qeros.js
+++ _assets/js/views/Qeros.js
@@ -14,12 +14,13 @@ define([
     beforeAppend: function() {
       if (Modernizr.video && Detectizr.device.model !== "iphone") {
         this.$el.find('.no-video').remove();
-        this.$el.find('.video').next().addClass('post-video');
+        // this.$el.find('video').remove();
       } else {
-        this.$el.find('.video').remove();
+        this.$el.find('video').remove();
+        // this.$el.find('.no-video').remove();
       }
       $('body').on('touchend', function(){
-        $('video')[0].play();
+        $('video').play();
       });
       this.$el.find('div[data-src]').each(function(i, el){
         this.preloadImg(el);
@@ -54,7 +55,10 @@ define([
         var tl = new TimelineLite({paused: true});
         
         tl.call(function(){
-          this.preloadVideo($('.bg .offering video')[0]);
+          var video = $('.bg .offering video')[0];
+          if (video) {
+            this.preloadVideo(video);
+          }
         }.bind(this));
         
         tl.to( this.$el.find('#skywalk .move-h div'), 10, {x:'-10%', ease:Sine.easeIn} );
@@ -73,7 +77,7 @@ define([
         tl.from( this.$el.find('#offering .move-h div, #offering .move-v div'), 1, {opacity:0}, 1 );
         
         tl.to(this.$el.find('.bg .offering'), 2.5, {opacity:1}, 1.5);
-        tl.to(this.$el.find('#offering .big-text div'), 5, {y:'70%', ease:Sine.easeOut}, 1.5);
+        tl.to(this.$el.find('#offering .big-text div'), 10, {y:'180%', ease:Sine.easeOut}, 0);
         tl.fromTo(this.$el.find('#offering .big-text p'), 1.5, {color:'#F22E60'}, {color:'#fafaff'}, 2);
 
         return tl;
@@ -93,7 +97,10 @@ define([
         var tl = new TimelineLite({paused: true});
 
         tl.call(function(){
-          this.preloadVideo($('.bg .where video')[0]);
+          var video = $('.bg .where video')[0];
+          if (video) {
+            this.preloadVideo(video);
+          }
         }.bind(this));
 
         tl.to( this.$el.find('#love .move-h div'), 10, {x:'-10%', ease:Sine.easeIn}, 0);
@@ -106,10 +113,13 @@ define([
       var createWhereTL = function() {
         var tl = new TimelineLite({paused: true});
 
-        tl.to(this.$el.find('.bg .where'), 5, {opacity:1});
-        tl.to(this.$el.find('#where .big-text div'), 5, {y:'60%'}, 1);
-        tl.fromTo(this.$el.find('#where .big-text p, #where .big-text h2'), 2.5, {color:'#F22E60'}, {color:'#fafaff'}, 2.5);
-        tl.to(this.$el.find('.bg .offering'), 5, {});
+        tl.to( this.$el.find('#where .move-h div'), 10, {x:'-10%', ease:Sine.easeIn}, 0);
+        tl.to( this.$el.find('#where .move-v div'), 10, {y:'-30%'}, 0 );
+        tl.from( this.$el.find('#where .move-h div, #where .move-v div'), 1, {opacity:0}, 1 );
+        
+        tl.to(this.$el.find('.bg .where'), 2.5, {opacity:1}, 1.5);
+        tl.to(this.$el.find('#where .big-text div'), 10, {y:'110%', ease:Sine.easeOut}, 0);
+        tl.fromTo(this.$el.find('#where .big-text p, #where .big-text h2'), 1.5, {color:'#F22E60'}, {color:'#fafaff'}, 2);
         
         return tl;
       }.bind(this);
@@ -164,115 +174,7 @@ define([
 
 
     render: function(currentScrollY){
-
       this.seekTimelines(currentScrollY);
-/*
-      var pageHeight = $('#content').innerHeight();
-      var pageWidth = $('#content').innerWidth();
-      var windowHeight = $(window).innerHeight();
-      var st = Math.max(0, currentScrollY);
-
-      $('.multiply .container').css('transform', 'translateY(' + -.95 * currentScrollY + 'px)');
-
-      $('.page section').each(function(i,el){
-        var $el = $(el);
-        var $imgEl = $el.find('.frame-img div, .bg .img, video');
-        var $captionEl = $el.find('.caption');
-        if (!$imgEl[0]) {
-          return;
-        }
-
-        var elTop = $el.offset().top;
-        var imgTop = $imgEl.offset().top;
-        var elHeight = $el.outerHeight();
-        var imgHeight = $imgEl.outerHeight();
-        var viewBottom = st + windowHeight;
-
-        var rangeMin = imgTop;
-        var rangeMax = imgTop + imgHeight + windowHeight;
-
-        var ratio = (viewBottom - rangeMin) / (rangeMax - rangeMin);
-
-        // Hide the top section if it's not scrolled into view
-        if ($el.attr('id') === 'top') {
-          var topRatio = (viewBottom - windowHeight) / (windowHeight * 2);
-          if (topRatio < -0.2 || topRatio > 1) {
-            $el.css({opacity: 0});
-          } else {
-            $el.css({opacity: 1});
-          }
-          return;
-        }
-
-        if (ratio < -0.2 || ratio > 1) {
-          return;
-        }
-
-        var opacityRatio = Math.max(0, Math.min(1, ratio * 3.5  -.3));
-        
-        if (pageWidth >= 768) {
-          $captionEl.css({
-            transform: 'translateY(' + ratio * 25 + '%)',  
-          });
-        }
-
-        // $captionEl.children().css({
-        //   opacity: Math.min(1, ratio * 3 - .5)
-        // });
-
-
-        if ($imgEl.parent().hasClass('move-h')) {
-          $imgEl.css({transform: 'translateX(-' + ratio * 8 + '%)'});
-          if (Detectizr.os.name === 'ios' && Detectizr.os.version.major < 8) {
-            return;
-          } else {
-            $imgEl.css({opacity: opacityRatio});
-          }
-        } 
-        if ($imgEl.parent().hasClass('move-v')) {
-          $imgEl.css({transform: 'translateY(-' + ratio * 15 + '%)'});
-          if (Detectizr.os.name === 'ios' && Detectizr.os.version.major < 8) {
-            return;
-          } else {
-            $imgEl.css({opacity: opacityRatio});
-          }
-        }
-
-        if (i === 1) {
-          var $stringEl = $('.string');
-          $stringEl.css({transform: 'translateY(' + ratio * 10 + '%)'});
-        }
-
-        if ($el.hasClass('video')) {
-          rangeMin = elTop;
-          rangeMax = elTop + imgHeight + windowHeight;
-          transformRatio = (viewBottom - rangeMin) / (rangeMax - rangeMin);
-
-          rangeMin = elTop + elHeight * 0.33;
-          rangeMax = elTop + elHeight * 0.4;
-          opacityRatio = (viewBottom - rangeMin) / (rangeMax - rangeMin);
-
-          rangeMin = elTop - windowHeight;
-          rangeMax = elTop + elHeight + windowHeight;
-          captionRatio = (viewBottom - rangeMin) / (rangeMax - rangeMin);
-          $captionEl.css({transform: 'translateY(' + Math.max(0, captionRatio) * 200 + '%)'});
-
-          scale = chroma.scale(['F22E60', '#fff']).mode('lab');
-          $captionEl.children('p').css({color: scale(opacityRatio).hex()});
-          
-          rangeMin = elTop + elHeight * 0.8;
-          rangeMax = elTop + elHeight;
-          opacityRatio2 = (viewBottom - rangeMin) / (rangeMax - rangeMin);
-
-          $imgEl.css({
-            transform: 'translateX(-50%) translateY(-' + transformRatio * 8 + '%)',
-            opacity: opacityRatio2 > 0 ? (1 - opacityRatio2) : opacityRatio/2
-          });
-        }
-
-
-      });
-*/
     }
   });
 
